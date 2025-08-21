805f9fd5835f3a24b6527f0cb86a2cd502363f7c
Dilshod Zopirov <dilshodzopirov@gmail.com>
2025-08-21 10:18:13 +0500

    Move RemoteFeedLoader component to production code

---
+++ EssentialFeed/EssentialFeed/FeedAPI/RemoteFeedLoader.swift
@@ -0,0 +1,26 @@
+//
+//  RemoteFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Dilshod Zopirov on 8/21/25.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    func get(from url: URL)
+}
+
+public class RemoteFeedLoader {
+    private let url: URL
+    private let client: HTTPClient
+    
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    public func load() {
+        client.get(from: url)
+    }
+}
