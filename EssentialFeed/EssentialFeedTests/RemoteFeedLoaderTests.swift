2aecbfa394457ce27898cad15a3fb9d0feb224c7
Dilshod Zopirov <dilshodzopirov@gmail.com>
2025-08-21 19:12:34 +0500

    Delivers invalid data error on non 200 responses

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -46,6 +46,16 @@ final class RemoteFeedLoaderTests: XCTestCase {
         XCTAssertEqual(capturedErrors, [RemoteFeedLoader.Error.connectivity])
     }
     
+    func test_load_deliversErrorOnNon200HttpResponse() {
+        let (sut, client) = makeSUT()
+        
+        var capturedErrors: [RemoteFeedLoader.Error] = []
+        sut.load { capturedErrors.append($0) }
+        client.complete(withStatusCode: 400)
+        
+        XCTAssertEqual(capturedErrors, [RemoteFeedLoader.Error.invalidData])
+    }
+    
     // MARK: Helpers
     
     private func makeSUT(url: URL = URL(string: "http://www.a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
@@ -55,17 +65,28 @@ final class RemoteFeedLoaderTests: XCTestCase {
     }
     
     private class HTTPClientSpy: HTTPClient {
-        var messages: [(url: URL, completion: (Error) -> Void)] = []
+        var messages: [(url: URL, completion: (Error?, HTTPURLResponse?) -> Void)] = []
         var requestedURLs: [URL] {
             messages.map { $0.url }
         }
         
-        func get(from url: URL, completion: @escaping (Error) -> Void) {
+        func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void) {
             messages.append((url, completion))
         }
         
         func complete(with error: Error, at index: Int = 0) {
-            messages[index].completion(error)
+            messages[index].completion(error, nil)
+        }
+        
+        func complete(withStatusCode code: Int, at index: Int = 0) {
+            let response = HTTPURLResponse(
+                url: requestedURLs[index],
+                statusCode: code,
+                httpVersion: nil,
+                headerFields: nil
+            )
+            
+            messages[index].completion(nil, response)
         }
     }
     
