1552ba2448cdd55dfd311ef51ffaddc89a74e87b
Dilshod Zopirov <dilshodzopirov@gmail.com>
2025-09-01 14:13:58 +0500

    Extract insert logic to a separate method to simplify save method

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -23,12 +23,16 @@ class LocalFeedLoader {
             if let error {
                 completion(error)
             } else {
-                self.store.insert(items: items, timestamp: currentDate(), completion: { [weak self] error in
-                    guard self != nil else { return }
-                    completion(error)
-                })
+                self.cache(items, with: completion)
             }
         }
+    }
+    
+    private func cache(_ items: [FeedItem], with completion: @escaping (Error?) -> Void) {
+        store.insert(items: items, timestamp: currentDate(), completion: { [weak self] error in
+            guard self != nil else { return }
+            completion(error)
+        })
     }
 }
 
