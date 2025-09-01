2266e4ff638d7f83c5de54087dfa7e28e11d80e7
Dilshod Zopirov <dilshodzopirov@gmail.com>
2025-09-01 11:02:05 +0500

    LocalFeedStrore request cache deletion on insertion

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -6,9 +6,26 @@
 //
 
 import XCTest
+import EssentialFeed
 
 class FeedStore {
     var deleteCallCount = 0
+    
+    func deleteCache() {
+        deleteCallCount += 1
+    }
+}
+
+class LocalFeedLoader {
+    let store: FeedStore
+    
+    init(store: FeedStore) {
+        self.store = store
+    }
+    
+    func save(items: [FeedItem]) {
+        store.deleteCache()
+    }
 }
 
 class CacheFeedUseCaseTests: XCTestCase {
@@ -16,6 +33,25 @@ class CacheFeedUseCaseTests: XCTestCase {
     func test_doesNotRequestDeleteOnCreation() {
         let store = FeedStore()
         XCTAssertEqual(store.deleteCallCount, 0)
+    }
+    
+    func test_save_requestCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        
+        sut.save(items: [uniqueItem(), uniqueItem()])
+        
+        XCTAssertEqual(store.deleteCallCount, 1)
+    }
+    
+    // MARK: Helpers
+    
+    private func uniqueItem() -> FeedItem {
+        return FeedItem(id: UUID(), description: "any", location: "any", imageURL: anyURL())
+    }
+    
+    private func anyURL() -> URL {
+        return URL(string: "http://any-url.com")!
     }
     
 }
