2ecbe1dc057720a1cc15dd62cf78367fee6ee752
Dilshod Zopirov <dilshodzopirov@gmail.com>
2025-09-02 18:29:52 +0500

    Load command delivers images on less than 7 days old cache

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -31,11 +31,14 @@ public final class LocalFeedLoader {
     }
     
     public func load(completion: @escaping (LoadResult) -> Void) {
-        store.retrieve { error in
-            if let error {
-                completion(.failure(error))
-            } else {
+        store.retrieve { result in
+            switch result {
+            case let .found(feed, _):
+                completion(.success(feed.toModel()))
+            case .empty:
                 completion(.success([]))
+            case let .failure(error):
+                completion(.failure(error))
             }
         }
     }
@@ -52,6 +55,14 @@ private extension Array where Element == FeedImage {
     func toLocal() -> [LocalFeedImage] {
         map {
             LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url)
+        }
+    }
+}
+
+private extension Array where Element == LocalFeedImage {
+    func toModel() -> [FeedImage] {
+        map {
+            FeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url)
         }
     }
 }
