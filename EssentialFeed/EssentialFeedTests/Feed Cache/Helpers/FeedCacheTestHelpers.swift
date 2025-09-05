47d56f62be4f44eebcfb890cc5663cf26f8f22c8
Dilshod Zopirov <dilshodzopirov@gmail.com>
2025-09-05 11:35:08 +0500

    Move helper functions to a shared scope to remove duplicate code

---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
@@ -0,0 +1,40 @@
+//
+//  FeedCacheTestHelpers.swift
+//  EssentialFeed
+//
+//  Created by Dilshod Zopirov on 9/5/25.
+//
+
+import Foundation
+import EssentialFeed
+
+func uniqueImage() -> FeedImage {
+    return FeedImage(id: UUID(), description: "any", location: "any", url: anyURL())
+}
+
+func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
+    let models = [uniqueImage(), uniqueImage()]
+    let local = models.map {
+        LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url)
+    }
+    
+    return (models, local)
+}
+
+func anyURL() -> URL {
+    return URL(string: "http://any-url.com")!
+}
+
+func anyNSError() -> NSError {
+    return NSError(domain: "any error", code: 0)
+}
+
+extension Date {
+    func adding(days: Int) -> Date {
+        return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
+    }
+    
+    func adding(seconds: Int) -> Date {
+        return Calendar(identifier: .gregorian).date(byAdding: .second, value: seconds, to: self)!
+    }
+}
