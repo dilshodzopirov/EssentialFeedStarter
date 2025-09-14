3fec671de60e52f5f7c3572cbea7c156baf00cdf
Dilshod Zopirov <dilshodzopirov@gmail.com>
2025-09-14 18:03:33 +0500

    Extract pull to refresh to a helper method

--- EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -75,3 +75,14 @@ class FeedViewControllerTests: XCTestCase {
     }
 
 }
+
+private extension UIRefreshControl {
+    func simulatePullToRefresh() {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: .valueChanged)?
+                .forEach { action in
+                    (target as NSObject).perform(Selector(action))
+                }
+        }
+    }
+}
