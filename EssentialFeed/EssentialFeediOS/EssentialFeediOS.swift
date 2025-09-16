a9953ffc0a1a333b9f20a4e5be2be67049321ade
Dilshod Zopirov <dilshodzopirov@gmail.com>
2025-09-16 23:29:27 +0500

    Move FeedViewController to its own file in production

--- EssentialFeed/EssentialFeediOS/EssentialFeediOS.swift
+++
@@ -1,9 +0,0 @@
-//
-//  EssentialFeediOS.swift
-//  EssentialFeediOS
-//
-//  Created by Dilshod Zopirov on 9/14/25.
-//
-
-import Foundation
-
--- EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -7,39 +7,7 @@
 
 import XCTest
 import EssentialFeed
-
-final class FeedViewController: UITableViewController {
-    private var loader: FeedLoader?
-    private var isViewAlreadyLoaded = false
-    
-    convenience init(loader: FeedLoader) {
-        self.init()
-        self.loader = loader
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        refreshControl = UIRefreshControl()
-        refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
-    }
-    
-    override func viewIsAppearing(_ animated: Bool) {
-        super.viewIsAppearing(animated)
-        
-        if !isViewAlreadyLoaded {
-            load()
-            isViewAlreadyLoaded = true
-        }
-    }
-    
-    @objc private func load() {
-        refreshControl?.beginRefreshing()
-        loader?.load { [weak self] _ in
-            self?.refreshControl?.endRefreshing()
-        }
-    }
-}
+import EssentialFeediOS
 
 class FeedViewControllerTests: XCTestCase {
     
