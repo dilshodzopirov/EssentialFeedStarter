b94c8f717fc325ae477ec3e20efabf67b9852f5c
Dilshod Zopirov <dilshodzopirov@gmail.com>
2025-09-28 15:57:00 +0500

    Move ViewModels to separate files

--- EssentialFeed/EssentialFeediOS/Feed UI/Feed Presentation/FeedImagePresenter.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Feed Presentation/FeedImagePresenter.swift
@@ -5,18 +5,6 @@
 import Foundation
 import EssentialFeed
 
-struct FeedImageViewModel<Image> {
-    let location: String?
-    let description: String?
-    let image: Image?
-    let isLoading: Bool
-    let shouldRetry: Bool
-    
-    var hasLocation: Bool {
-        return location != nil
-    }
-}
-
 protocol FeedImageView {
     associatedtype Image
     
---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  Created by Dilshod Zopirov on 9/28/25.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
