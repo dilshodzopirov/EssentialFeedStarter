410f5a1dddf59b2263b287e83e91811cdea40858
Dilshod Zopirov <dilshodzopirov@gmail.com>
2025-10-02 19:45:10 +0500

    Move view models to separate files

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  Created by Dilshod Zopirov on 10/2/25.
+//
+
+public struct FeedErrorViewModel {
+    public let message: String?
+    
+    static var noError: FeedErrorViewModel {
+        return FeedErrorViewModel(message: nil)
+    }
+    
+    static func error(message: String) -> FeedErrorViewModel {
+        return FeedErrorViewModel(message: message)
+    }
+}
---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,7 @@
+//
+//  Created by Dilshod Zopirov on 10/2/25.
+//
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
