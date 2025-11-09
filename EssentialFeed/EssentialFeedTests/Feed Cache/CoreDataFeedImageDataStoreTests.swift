5646f5aed866ad736a231c5a8f601dfd081be6a3
Dilshod Zopirov <dilshodzopirov@gmail.com>
2025-11-10 01:11:29 +0500

    CoreDataFeedStore.retriveImageData deliver image data not found when store is not empty but no image data found for matching url

--- EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
@@ -5,12 +5,12 @@
 import XCTest
 import EssentialFeed
 
-final class CoreDataFeedImageDataStore: FeedImageDataStore {
-    func insert(_ data: Data, for url: URL, completion: @escaping (InsertionResult) -> Void) {
+extension CoreDataFeedStore: FeedImageDataStore {
+    public func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
         
     }
     
-    func retrieve(dataForURL url: URL, completion: @escaping (RetrievalResult) -> Void) {
+    public func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
         completion(.success(.none))
     }
 }
@@ -18,15 +18,27 @@ final class CoreDataFeedImageDataStore: FeedImageDataStore {
 final class CoreDataFeedImageDataStoreTests: XCTestCase {
     
     func test_retrieveImageData_deliversNotFoundWhenEmpty() {
-        let sut = CoreDataFeedImageDataStore()
+        let sut = makeSUT()
         
         expect(sut, toCompleteRetrievalWith: notFound(), for: anyURL())
     }
     
+    func test_retrieveImageData_deliversNotFoundWhenNoEmptyButNoDataForURL() {
+        let sut = makeSUT()
+        let url = URL(string: "http://a-url.com")!
+        let nonMatchingURL = URL(string: "http://non-matching-url.com")!
+
+        insert(anyData(), for: url, into: sut)
+        
+        expect(sut, toCompleteRetrievalWith: notFound(), for: nonMatchingURL)
+    }
+    
     // MARK: Helpers
     
-    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> CoreDataFeedImageDataStore {
-        let sut = CoreDataFeedImageDataStore()
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> CoreDataFeedStore {
+        let storeBundle = Bundle(for: CoreDataFeedStore.self)
+        let storeURL = URL(fileURLWithPath: "/dev/null")
+        let sut = try! CoreDataFeedStore(storeURL: storeURL, bundle: storeBundle)
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
     }
@@ -35,8 +47,12 @@ final class CoreDataFeedImageDataStoreTests: XCTestCase {
         return .success(.none)
     }
     
+    private func localImage(url: URL) -> LocalFeedImage {
+        LocalFeedImage(id: UUID(), description: "any", location: "any", url: url)
+    }
+    
     private func expect(
-        _ sut: CoreDataFeedImageDataStore,
+        _ sut: CoreDataFeedStore,
         toCompleteRetrievalWith expectedResult: FeedImageDataStore.RetrievalResult,
         for url: URL,
         file: StaticString = #filePath,
@@ -50,6 +66,33 @@ final class CoreDataFeedImageDataStoreTests: XCTestCase {
             default:
                 XCTFail("Expected \(expectedResult), got \(receivedResult) instead.", file: file, line: line)
             }
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 0.1)
+    }
+    
+    private func insert(
+        _ data: Data,
+        for url: URL,
+        into sut: CoreDataFeedStore,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) {
+        let exp = expectation(description: "Wait for cache insertion")
+        let image = localImage(url: url)
+        sut.insert([image], timestamp: Date()) { result in
+            switch result {
+            case let .failure(error):
+                XCTFail("Failed to save \(image) with error \(error)", file: file, line: line)
+            case .success:
+                sut.insert(data, for: url) { result in
+                    if case let Result.failure(error) = result {
+                        XCTFail("Failed to insert \(data) with error \(error)", file: file, line: line)
+                    }
+                }
+            }
+            
             exp.fulfill()
         }
         
