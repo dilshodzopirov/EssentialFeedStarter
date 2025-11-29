e0899bd0920674b8738e52b69c3bb88cd96cb3dd
Dilshod Zopirov <dilshodzopirov@gmail.com>
2025-11-30 02:45:19 +0500

    FeedImageDataLoaderWithFallbackComposite.loadImageData delivers primary image data on primary load image data success

---
+++ EssentialApp/EssentialAppTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
@@ -0,0 +1,94 @@
+import XCTest
+import EssentialFeed
+
+final class FeedImageDataLoaderWithFallbackComposite: FeedImageDataLoader {
+    private let primaryLoader: FeedImageDataLoader
+    private let fallbackLoader: FeedImageDataLoader
+    
+    private struct Task: FeedImageDataLoaderTask {
+        func cancel() {
+            
+        }
+    }
+    
+    init(primaryLoader: FeedImageDataLoader, fallbackLoader: FeedImageDataLoader) {
+        self.primaryLoader = primaryLoader
+        self.fallbackLoader = fallbackLoader
+    }
+    
+    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        return primaryLoader.loadImageData(from: url, completion: completion)
+    }
+}
+
+final class FeedImageDataLoaderWithFallbackCompositeTests: XCTestCase {
+    
+    func test_loadImageData_deliversPrimaryImageDataOnPrimaryLoaderSuccess() {
+        let primaryData = Data("primary".utf8)
+        let fallbackData = Data("fallback".utf8)
+
+        let sut = makeSUT(primaryResult: .success(primaryData), fallbackResult: .success(fallbackData))
+        
+        expect(sut, toCompleteWith: .success(primaryData))
+    }
+    
+    // MARK: Helpers
+    
+    private func makeSUT(primaryResult: FeedImageDataLoader.Result, fallbackResult: FeedImageDataLoader.Result, file: StaticString = #filePath, line: UInt = #line) -> FeedImageDataLoaderWithFallbackComposite {
+        let primaryLoader = LoaderStub(result: primaryResult)
+        let fallbackLoader = LoaderStub(result: fallbackResult)
+        let sut = FeedImageDataLoaderWithFallbackComposite(primaryLoader: primaryLoader, fallbackLoader: fallbackLoader)
+        trackForMemoryLeaks(primaryLoader, file: file, line: line)
+        trackForMemoryLeaks(fallbackLoader, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
+    
+    private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
+        }
+    }
+    
+    private func expect(_ sut: FeedImageDataLoader, toCompleteWith expectedResult: FeedImageDataLoader.Result, file: StaticString = #filePath, line: UInt = #line) {
+        let exp = expectation(description: "Wait for completion")
+        let _ = sut.loadImageData(from: anyURL()) { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedData), .success(expectedData)):
+                XCTAssertEqual(receivedData, expectedData, file: file, line: line)
+            case (.failure, .failure):
+                break
+            default:
+                XCTFail("Expected \(expectedResult), got \(receivedResult) instead", file: file, line: line)
+            }
+            
+            exp.fulfill()
+        }
+         
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    private func anyURL() -> URL {
+        return URL(string: "https://any-url.com")!
+    }
+    
+    private final class LoaderStub: FeedImageDataLoader {
+        private let result: FeedImageDataLoader.Result
+        
+        init(result: FeedImageDataLoader.Result) {
+            self.result = result
+        }
+        
+        private struct Task: FeedImageDataLoaderTask {
+            func cancel() {
+                
+            }
+        }
+        
+        func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+            completion(result)
+            return Task()
+        }
+    }
+    
+}
