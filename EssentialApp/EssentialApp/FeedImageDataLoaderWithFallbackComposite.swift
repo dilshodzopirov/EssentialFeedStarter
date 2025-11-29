3b60f148f6d7caec63923ff25c0b981e3bd47f32
Dilshod Zopirov <dilshodzopirov@gmail.com>
2025-11-30 03:00:49 +0500

    Create Task for FeedImageDataLoaderWithFallbackComposite

--- EssentialApp/EssentialApp/FeedImageDataLoaderWithFallbackComposite.swift
+++ EssentialApp/EssentialApp/FeedImageDataLoaderWithFallbackComposite.swift
@@ -5,9 +5,19 @@ public final class FeedImageDataLoaderWithFallbackComposite: FeedImageDataLoader
     private let primaryLoader: FeedImageDataLoader
     private let fallbackLoader: FeedImageDataLoader
     
-    private struct Task: FeedImageDataLoaderTask {
+    private final class Task: FeedImageDataLoaderTask {
+        private var completion: ((FeedImageDataLoader.Result) -> Void)?
+        
+        init(completion: ((FeedImageDataLoader.Result) -> Void)? = nil) {
+            self.completion = completion
+        }
+        
         func cancel() {
-            
+            completion = nil
+        }
+        
+        func complete(with result: FeedImageDataLoader.Result) {
+            completion?(result)
         }
     }
     
@@ -17,13 +27,19 @@ public final class FeedImageDataLoaderWithFallbackComposite: FeedImageDataLoader
     }
     
     public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-        return primaryLoader.loadImageData(from: url) { [weak self] result in
+        let task = Task(completion: completion)
+        
+        _ = primaryLoader.loadImageData(from: url) { [weak self] result in
             switch result {
             case .success:
-                completion(result)
+                task.complete(with: result)
             case .failure:
-                _ = self?.fallbackLoader.loadImageData(from: url, completion: completion)
+                _ = self?.fallbackLoader.loadImageData(from: url) { result in
+                    task.complete(with: result)
+                }
             }
         }
+        
+        return task
     }
 }
